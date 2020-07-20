4f61364bb8735d01594e61f55f0242f7f07b748c
Bokhoo <13770620@qq.com>
2020-07-21 00:30:30 +0800

    commit test1

--- src/com/bjpowernode/domain/Persion.java
+++ src/com/bjpowernode/domain/Persion.java
@@ -1,9 +1,46 @@
 package com.bjpowernode.domain;
 
+import java.util.Date;
+
 /**
  * @author zbh
  * @date 2020/7/20
  */
 public class Persion {
     private String id;
+    private String name;
+    private int age;
+    private Date date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
